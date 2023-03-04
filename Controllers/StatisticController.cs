1bacf8b84190f568305df41670eca365d1c7f49e
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-04 12:24:16 +0300

    Authorize

---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transportation.Models;
+
+namespace Transportation.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbTransportEntities2 db = new DbTransportEntities2 ();
+        public ActionResult Index()
+        {
+            ViewBag.customerCount = db.TblCustomer.Count();
+
+            ViewBag.cityAnkara = db.TblCustomer.Where(x => x.CustomerCity == "Ankara").Count();
+
+            ViewBag.categoryCount = db.TblCategory.Count();
+
+            ViewBag.citySelect = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity);
+
+            return View();
+        }
+    }
+}

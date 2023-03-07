c67a5ccac5b414ccb286550b691b3653ee6c7fdc
Muhammet Dobaoğlu <106467744+Muhammet-Dobaoglu@users.noreply.github.com>
2023-03-07 11:11:12 +0300

    Query

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -12,14 +12,13 @@ namespace Transportation.Controllers
         DbTransportEntities2 db = new DbTransportEntities2 ();
         public ActionResult Index()
         {
-            ViewBag.customerCount = db.TblCustomer.Count();
-
+             ViewBag.customerCount = db.TblCustomer.Count();
             ViewBag.cityAnkara = db.TblCustomer.Where(x => x.CustomerCity == "Ankara").Count();
-
             ViewBag.categoryCount = db.TblCategory.Count();
-
-            ViewBag.citySelect = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity).FirstOrDefault();
-
+            //ViewBag.citySelect = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity);
+            ViewBag.employeeName = db.TblEmployee.Where(x => x.EmployeeID == 1).Select(y => y.EmployeeName).SingleOrDefault();
+            ViewBag.urunSayısı = db.TblProduct.Count();
+            ViewBag.cityName = db.TblCustomer.Where(x => x.CustomerID == 1).Select(y => y.CustomerCity).SingleOrDefault();
             return View();
         }
     }
